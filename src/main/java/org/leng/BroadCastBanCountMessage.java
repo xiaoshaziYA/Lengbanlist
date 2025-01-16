9aac0b374544d343c904d26ec566266c3c9afb5e
Leng <110669856+xiaoshaziYA@users.noreply.github.com>
2025-01-16 17:09:28 +0800

    Update BroadCastBanCountMessage.java
    让广播支持彩色命令，在config中修改即可&开头！

--- src/main/java/org/leng/BroadCastBanCountMessage.java
+++ src/main/java/org/leng/BroadCastBanCountMessage.java
@@ -1,14 +1,20 @@
 package org.leng;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class BroadCastBanCountMessage extends BukkitRunnable {
     @Override
     public void run() {
-        if (!LengbanList.getInstance().getServer().getOnlinePlayers().isEmpty()){
-            for (Player player : LengbanList.getInstance().getServer().getOnlinePlayers()){
-                player.sendMessage(LengbanList.getInstance().getConfig().getString("default-message").replace("%s", String.valueOf(LengbanList.getInstance().banManager.getBanList().size())));
+        if (!LengbanList.getInstance().getServer().getOnlinePlayers().isEmpty()) {
+            String message = LengbanList.getInstance().getConfig().getString("default-message");
+            int banCount = LengbanList.getInstance().banManager.getBanList().size();
+            message = message.replace("%s", String.valueOf(banCount));
+            message = ChatColor.translateAlternateColorCodes('&', message); // 解析颜色代码
+
+            for (Player player : LengbanList.getInstance().getServer().getOnlinePlayers()) {
+                player.sendMessage(message);
             }
         }
     }
