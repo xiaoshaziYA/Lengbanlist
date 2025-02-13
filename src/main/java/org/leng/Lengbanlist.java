2a206438852a062af32b6bfc9ac52eab2c4dd302
darkpath2011 <3120931286@qq.com>
2025-02-13 18:30:09 +0800

    Add IP query and save functionality, optimize imports and config

--- src/main/java/org/leng/Lengbanlist.java
+++ src/main/java/org/leng/Lengbanlist.java
@@ -2,14 +2,17 @@ package org.leng;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandMap;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.leng.commands.BanCommand;
 import org.leng.commands.LengbanlistCommand;
+import org.leng.commands.QuaryIPCommand;
 import org.leng.commands.UnbanCommand;
 import org.leng.manager.BanManager;
 import org.leng.manager.ModelManager;
 import org.leng.utils.GitHubUpdateChecker;
+import org.leng.utils.YamlLoaderUtil;
 
 import java.lang.reflect.Field;
 
@@ -18,10 +21,12 @@ public class Lengbanlist extends JavaPlugin {
     public BanManager banManager;
     public BukkitTask task;
     private boolean isBroadcast;
+    public FileConfiguration ipFC;
 
     @Override
     public void onLoad() {
         saveDefaultConfig();
+        ipFC = YamlLoaderUtil.loadCustomYamlConfig(this,"ip.yml");
         instance = this;
         banManager = new BanManager();
         isBroadcast = getConfig().getBoolean("opensendtime");
@@ -32,11 +37,17 @@ public class Lengbanlist extends JavaPlugin {
         getServer().getConsoleSender().sendMessage(prefix() + "§f§2正在加载原神");
         // 初始化模型管理器，加载当前配置中指定的模型
         ModelManager.getInstance();
-        getServer().getConsoleSender().sendMessage(prefix() + "§f§2传送锚点已解锁，当前Model: " + ModelManager.getInstance().getCurrentModelName());
+        getServer().getConsoleSender().sendMessage(prefix() + "§f§2传送锚点已解锁，当前Model: " + ModelManager.getCurrentModelName());
         getServer().getPluginManager().registerEvents(new Listener(), this);
         getCommandMap().register("", new LengbanlistCommand("lban", this));
         getCommandMap().register("", new BanCommand());
         getCommandMap().register("", new UnbanCommand());
+        getCommandMap().register("", new QuaryIPCommand());
+        new Metrics(this, 24495);
+        GitHubUpdateChecker.checkUpdata();
+        if (isBroadcast) {
+            task = new BroadCastBanCountMessage().runTaskTimer(Lengbanlist.getInstance(), 0L, getConfig().getInt("sendtime") * 1200L);
+        }
         getServer().getConsoleSender().sendMessage("§b  _                      ____              _      _     _   ");
         getServer().getConsoleSender().sendMessage("§6 | |                    |  _ \\            | |    (_)   | |  ");
         getServer().getConsoleSender().sendMessage("§b | |     ___ _ __   __ _| |_) | __ _ _ __ | |     _ ___| |_ ");
@@ -48,11 +59,6 @@ public class Lengbanlist extends JavaPlugin {
         getServer().getConsoleSender().sendMessage("§b当前运行版本：v1.3.5");
         getServer().getConsoleSender().sendMessage("§b当前运行在：" + Bukkit.getServer().getVersion());
         getServer().getConsoleSender().sendMessage("§b赞助获得更多福利:https://afdian.com/a/lengbanlist");
-        new Metrics(this, 24495);
-        GitHubUpdateChecker.checkUpdata();
-        if (isBroadcast) {
-            task = new BroadCastBanCountMessage().runTaskTimer(Lengbanlist.getInstance(), 0L, getConfig().getInt("sendtime") * 1200L);
-        }
     }
 
     @Override
